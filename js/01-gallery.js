7a27cc82736c38e593bccd61521421d4696eb498
Alex-Sorokolit <sorokolital@gmail.com>
2022-09-13 18:46:24 +0300

    render page

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery (items) {
+ return items
+ .map(({preview, original, description})=> {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+  `
+ }).join(' ');
+}
+console.log(createGallery(galleryItems));
+
